e30320142d4e61eda97cf88c44d0d5b04b0c2220
Joan Matas Enjuanes <joan.matas@estudiantat.upc.edu>
2022-03-22 16:04:59 +0100

    22/3/22 v1

--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -10,8 +10,19 @@ using namespace std;
 namespace upc {
   void PitchAnalyzer::autocorrelation(const vector<float> &x, vector<float> &r) const {
 
-    for (unsigned int l = 0; l < r.size(); ++l) {
+    for (unsigned int l = 0; l < r.size(); l++) {
   		/// \TODO Compute the autocorrelation r[l]
+      /** 
+      \DONE Autocorrelation r[l] computed
+      - autocorrelation set to 0
+      - autocorrelation acumulated for all the signal
+      - autocorrelation divided by length
+      */
+      r[l] = 0;
+      for (unsigned int n = l; n < x.size(); n++) {
+        r[l] += x[n]*x[n-l];
+      }
+      r[l] /= x.size();
     }
 
     if (r[0] == 0.0F) //to avoid log() and divide zero 
@@ -27,6 +38,7 @@ namespace upc {
     switch (win_type) {
     case HAMMING:
       /// \TODO Implement the Hamming window
+      window.assign(frameLen, 1); //canviar per la de hamming
       break;
     case RECT:
     default:
